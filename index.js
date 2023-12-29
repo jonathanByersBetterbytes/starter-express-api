89e7add79886c39cc7833904a3986a960f5a483a
JP <jonathankbyers@gmail.com>
2023-12-29 17:26:48 -0500

    revert back

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ MongoClient.connect(dbConnectionStr)
 
 app.get('/',async (request, response)=>{
     console.log("Just got a request!")
-    const todoItems = await db.collection('todos').find().toArray()
-    ///const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+    //const todoItems = await db.collection('todos').find().toArray()
+    //const itemsLeft = await db.collection('todos').countDocuments({completed: false})
     
-    response.send('BetterBytes with MongoDB!'+todoItems)
+    response.send('BetterBytes with MongoDB!')
     //response.render('index.ejs', { items: todoItems, left: itemsLeft })
     // db.collection('todos').find().toArray()
     // .then(data => {
