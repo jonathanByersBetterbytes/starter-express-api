c527eee00619f0e232a49d7a0af9932e8d0acd5d
JP <jonathankbyers@gmail.com>
2023-12-29 16:51:17 -0500

    revert back

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 3000
+
+let db,
+    dbConnectionStr = 'mongodb+srv://jonathankbyers:02S6kDc3hv9J0oMX@testingcluster.wdoczch.mongodb.net',
+    dbName = 'rap'
+
+MongoClient.connect(dbConnectionStr)
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('BetterBytes!')
