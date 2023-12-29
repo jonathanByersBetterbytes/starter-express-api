09292f2b90a113a5e90f79e3db7b3f16869876a4
JP <jonathankbyers@gmail.com>
2023-12-29 16:53:52 -0500

    revert back

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 3000
+const cors = require('cors')
+require('dotenv').config()
+app.use(cors())
+app.set('view engine','ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
 
 let db,
     dbConnectionStr = 'mongodb+srv://jonathankbyers:02S6kDc3hv9J0oMX@testingcluster.wdoczch.mongodb.net',
