b0f088b076ef5f2ba44a39d4c38f4aeef3a010e0
JP <jonathankbyers@gmail.com>
2023-12-29 16:48:36 -0500

    revert back

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 3000
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('BetterBytes!')
 })
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || PORT)
