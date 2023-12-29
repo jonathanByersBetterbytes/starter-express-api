bacc7e11b3e3dad80e88be8e6cbfa83e849b2bbb
JP <jonathankbyers@gmail.com>
2023-12-29 16:57:58 -0500

    revert back

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 3000
+const cors = require('cors')
+app.use(cors())
 app.set('view engine','ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true}))
